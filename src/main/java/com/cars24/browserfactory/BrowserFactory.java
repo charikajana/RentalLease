9e7a52d09c80a07b7c4de681cde660d3ebf88fda
Veera Brahmachari <Chari.VeeraBrahmachari@sabre.com>
2021-05-26 18:28:12 +0530

    changes are udpated

--- src/main/java/com/cars24/browserfactory/BrowserFactory.java
+++ src/main/java/com/cars24/browserfactory/BrowserFactory.java
@@ -1,5 +1,8 @@
 package com.cars24.browserfactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,15 +13,20 @@ public class BrowserFactory {
 	static WebDriver driver;
 
 	public static WebDriver getbrowser(String BrowserName) {
+		String Browser = System.getProperty("Browser");
+		if (Browser != null) {
+			BrowserName = Browser;
+		}
 		switch (BrowserName.toUpperCase()) {
-		case "CHROME":			
-			driver=chromeBrowser();
+		case "CHROME":
+			driver = chromeBrowser();
 			break;
 		case "FIREFOX":
 			break;
 		case "IE":
 			break;
 		case "CHROMEMOBILEWEB":
+			driver = mobileEmulatorchromeBrowser();
 			break;
 		case "SAFARI":
 			break;
@@ -27,12 +35,27 @@ public class BrowserFactory {
 		}
 		return driver;
 	}
-	
-	
+
 	public static WebDriver chromeBrowser() {
-		ChromeOptions chromeOptions=new ChromeOptions();
+		ChromeOptions chromeOptions = new ChromeOptions();
 		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver(chromeOptions);
+		driver = new ChromeDriver(chromeOptions);
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+	public static WebDriver mobileEmulatorchromeBrowser() {
+		String MobileEmulator = System.getProperty("MobileEmulator");
+		Map<String, String> mobileEmulation = new HashMap<>();
+		if (MobileEmulator != null) {
+			mobileEmulation.put("deviceName", MobileEmulator);
+		} else {
+			mobileEmulation.put("deviceName", "");
+		}
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+		driver = new ChromeDriver(chromeOptions);
 		driver.manage().window().maximize();
 		return driver;
 	}
--- target/test-classes/features/RCStatus.feature
+++
@@ -1,8 +0,0 @@
-@FirstFeature
-Feature: Verify the RC Status
-
-  Scenario: Verify user able to login successfully
-    Given User open the browser and enter the URL
-    And click on RCStatus Link
- #   Then Verify the RC page title
-    And Close the browser
